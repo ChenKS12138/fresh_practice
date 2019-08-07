c1cfa84417e276d7ab2b2d1ce8d900271e52428c
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-08-07 23:11:03 +0800

    feat: init length 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,25 @@
 class Snake {
   constructor(canvasWidth) {
     this.canvasWidth = canvasWidth;
-    this.body = [22, 21];
+    this.body = [];
     this._directionCode = 2; // 1为上，2为下，3为左，4为右
     this.live = true;
     this._directionLock = false;
+    this.body[0] = parseInt(Math.random()*Math.pow(canvasWidth,2));
+    let i = 0 ;
+    while(this.length<4){
+      this.directionCode = [1,2,3,4][parseInt(Math.random()*4)];
+      if(this.body.includes(this.nextHead)||(this.nextHead+1)%canvasWidth === 0||this.nextHead>Math.pow(canvasWidth,2)||this.nextHead<0) return;
+      this.body.unshift(this.nextHead);
+      this._directionLock = false;
+      if(i++>10){
+        this.body[0] = parseInt(Math.random()*Math.pow(canvasWidth,2));
+        i=0;
+      }
+    }
+  }
+  get length(){
+    return this.body.length;
   }
   get score() {
     return this.body.length - 2;
